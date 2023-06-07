3e66ea982ee60b7e30d337d23a609500e3c8827d
pedrodeniz48 <pedrodeniz11@gmail.com>
2023-06-07 13:22:04 -0600

    [0.4.0] Drawing the trajectory with obstacles

--- Software/ComputerVision/vision_controller.py
+++ Software/ComputerVision/vision_controller.py
@@ -66,6 +66,13 @@ class ObjectDetection:
                 contour = cv2.approxPolyDP(cnt, 0.001*cv2.arcLength(cnt, True), True)
 
                 if orientation:
+                    """if robot:
+                        x,y,w,h = cv2.boundingRect(contour)
+                        cX = int(x+(w/2))
+                        cY = int(y+(h/2))
+                        
+                        self.center = [(cX, cY)]
+                        idx = 0"""
 
                     rect = cv2.minAreaRect(contour)
                     box = cv2.boxPoints(rect)
@@ -112,6 +119,182 @@ class ObjectDetection:
             self.binary_detection(ext_field[obs], 250, 1, 1)
             self.frame = frame
             self.rectangle_drawing(1, 100000, True)
+
+
+class TrajectoryPlanner:
+    
+    def __init__(self, frame=None, init_point = None, final_point = None, obstacle = None):
+        self.frame = frame
+        self.init_point = init_point
+        self.final_point = final_point
+        self.obstacle = obstacle
+
+        self.gray_frame = cv2.cvtColor(self.frame,cv2.COLOR_BGR2GRAY)
+        self.motions = [0, 0, 0, 0, 0] #turn_right, turn_left, right_walk, left_walk, forward
+
+    def linear_path(self):
+        self.line_mask = np.zeros_like(self.gray_frame)
+
+        #cv2.line(self.frame, self.init_point.center, self.final_point.center, (255, 255, 0), 2)
+        cv2.line(self.line_mask, self.init_point.center, self.final_point.center, (255, 255, 255), 1)
+    
+    def rotate_obstacles(self, masks):
+        or_obs = []
+        mid_f = []
+        ext_f = []
+        
+        for mask in range(0,len(self.obstacle.mask)):
+            obstacle_mask = np.zeros_like(self.gray_frame)
+
+            obstacle_mask, last_point = self._get_object_orientation(obstacle_mask, self.obstacle, mask)
+
+            ang = self._get_angle(self.obstacle.center[mask], (0, self.obstacle.center[mask][1]), last_point) #First point hast to be the intersection
+            #cv2.putText(self.frame, str(ang), (self.obstacle.center[mask][0]+10, self.obstacle.center[mask][1]-20), cv2.FONT_HERSHEY_COMPLEX, 0.7, (255, 255, 255), 1)
+
+            #cv2.imshow('mask', and_mask)
+            #cv2.imshow('video', frame)
+
+            if ang is not None:
+                original_obs = cv2.getRotationMatrix2D(self.obstacle.center[mask], ang-46, 1)
+                orig_rotation = cv2.warpAffine(self.obstacle.mask[mask], original_obs, (frame.shape[1], frame.shape[0]))
+
+                middle_field = cv2.getRotationMatrix2D(self.obstacle.center[mask], ang-46, 4)
+                mid_rotation = cv2.warpAffine(self.obstacle.mask[mask], middle_field, (frame.shape[1], frame.shape[0]))
+
+                ext_field = cv2.getRotationMatrix2D(self.obstacle.center[mask], ang-46, 5)
+                ext_rotation = cv2.warpAffine(self.obstacle.mask[mask], ext_field, (frame.shape[1], frame.shape[0]))
+
+                or_obs.append(orig_rotation)
+                mid_f.append(mid_rotation)
+                ext_f.append(ext_rotation)
+                #cv2.imshow('test', rotation)
+
+        return or_obs, mid_f, ext_f
+    
+    def draw_trajectory(self):
+        self.trajectory_mask = np.zeros_like(self.gray_frame)
+        self.displacement_mask = np.zeros_like(self.gray_frame)
+
+        points = []
+        points.append(self.final_point.center)
+        obs_pts = self._detect_obstacles()
+
+        for pt in obs_pts:
+            points.append(pt)
+
+        points.append(self.init_point.center)
+        
+        for i in range(0,len(points)-1):
+            cv2.line(self.trajectory_mask, points[i], points[i+1], (255, 255, 255), 2)
+            cv2.line(self.frame, points[i], points[i+1], (175, 255, 0), 2)
+
+            if i%2 == 0:
+                cv2.line(self.displacement_mask, points[i], points[i+1], (255, 255, 255), 2)
+
+    def _detect_obstacles(self):
+        self.obstacle.mid_boxes.reverse()
+
+        point_mask = cv2.bitwise_and(self.line_mask, self.obstacle.mid_field_mask)
+
+        contours, _ = cv2.findContours(point_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        centers = []
+        output_pts = []
+        
+        for cnt in contours:
+            contour = cv2.approxPolyDP(cnt, 0.001*cv2.arcLength(cnt, True), True)
+            x,y,w,h = cv2.boundingRect(contour)
+
+            center = (int(x+w/2), int(y+h/2))
+
+            centers.append(center)
+
+        for i in range(0,len(centers)):
+            if ((i+1) % 2 != 0):
+                
+                #cv2.circle(self.frame, centers[i], 10, (255,0,0), 2)
+
+                dst_right = self._get_distance(centers[i], tuple(self.obstacle.mid_boxes[int(i/2)][0][3].tolist()))
+                dst_left = self._get_distance(centers[i], tuple(self.obstacle.mid_boxes[int(i/2)][0][0].tolist()))
+                if dst_left < dst_right:
+                    #cv2.line(self.frame, centers[i], (tuple(self.obstacle.mid_boxes[i][0][0].tolist())), (0, 0, 255), 2)
+                    output_pts.append(centers[i])
+                    output_pts.append((tuple(self.obstacle.mid_boxes[int(i/2)][0][0].tolist())))
+                    #cv2.line(self.frame, tuple(self.obstacle.mid_boxes[i][0][0].tolist()), tuple(self.obstacle.mid_boxes[i][0][1].tolist()), (0, 0, 255), 2)
+                    #cv2.line(self.frame, tuple(self.obstacle.mid_boxes[i][0][1].tolist()), centers[i+1], (0, 0, 255), 2)
+                    output_pts.append((tuple(self.obstacle.mid_boxes[int(i/2)][0][1].tolist())))
+                    output_pts.append(centers[i+1])
+                else:
+                    #cv2.line(self.frame, centers[i], (tuple(self.obstacle.mid_boxes[i][0][3].tolist())), (0, 0, 255), 2)
+                    output_pts.append(centers[i])
+                    output_pts.append((tuple(self.obstacle.mid_boxes[int(i/2)][0][3].tolist())))
+                    #cv2.line(self.frame, tuple(self.obstacle.mid_boxes[i][0][3].tolist()), tuple(self.obstacle.mid_boxes[i][0][2].tolist()), (0, 0, 255), 2)
+                    #cv2.line(self.frame, tuple(self.obstacle.mid_boxes[i][0][2].tolist()), centers[i+1], (0, 0, 255), 2)
+                    output_pts.append((tuple(self.obstacle.mid_boxes[int(i/2)][0][2].tolist())))
+                    output_pts.append(centers[i+1])
+        
+        return output_pts
+    
+    def _get_object_orientation(self, mask=None, object=None, obj=None):
+        min_dist = 100000
+        box = object.box[obj]
+        points = [box[1], box[3]]
+
+        for cnt, point in enumerate(points):
+            dst = self._get_distance(box[0], point)
+            if dst < min_dist:
+                min_dist = dst
+                sec_pnt = points[cnt]
+                fth_pnt = points[not cnt]
+
+        low_mid = self._get_mid_point(box[0], sec_pnt)
+        high_mid = self._get_mid_point(box[2], fth_pnt)
+
+        slope =  self._get_slope(low_mid, high_mid)
+
+        height, width, _ = self.frame.shape
+
+        for x in range(0,width):
+            y=int(slope*(x-low_mid[0])+low_mid[1])
+            if(y>=0 and y<height):
+                cv2.circle(mask, (x,y), 1, (255,255,255))
+                last_point = (x, y)
+        
+        return mask, last_point
+
+    def _get_mid_point(self, pointA, pointB):
+        mid_point = []
+        for i in [0,1]:
+            mid_point.append(int((pointA[i]+pointB[i])/2))
+        return mid_point
+    
+    def _get_distance(self, initial_point, final_point):
+        if initial_point[0] == final_point[0]:
+            dst = final_point[1]-initial_point[1]
+        elif initial_point[1] == final_point[1]:
+            dst = final_point[0]-initial_point[0]
+        else:
+            dst =  math.sqrt((final_point[0]-initial_point[0])**2 + (final_point[1]-initial_point[1])**2) #(y2-y1)/(x2-x1)
+        return dst
+    
+    def _get_slope(self, initial_point, final_point):
+        if initial_point[0] == final_point[0]:
+            slope = 0
+        elif initial_point[1] == final_point[1]:
+            slope = 0
+        else:
+            slope = (final_point[1]-initial_point[1])/(final_point[0]-initial_point[0]) #(y2-y1)/(x2-x1)
+        return slope
+        
+    def _get_angle(self, pt1=None, pt2=None, pt3=None):
+        try:
+            m1 = self._get_slope(pt1, pt2)
+            m2 = self._get_slope(pt1, pt3)
+            angR = math.atan((m2-m1)/(1+(m2*m1)))
+            angD = round(math.degrees(angR))
+            return angD
+        except:
+            pass
                 
 
 
@@ -124,6 +307,9 @@ if __name__ == '__main__':
     pink_lower = (165, 125, 125)
     pink_upper = (175, 255, 255)
 
+    black_lower = (120, 75, 0)
+    black_upper = (140, 140, 50)
+
     green_lower = (50, 200, 0)
     green_upper = (85, 255, 255)
 
@@ -134,12 +320,12 @@ if __name__ == '__main__':
     blue_upper = (140, 255, 175)
 
     while True:
+
         ret, frame = shot.read()
 
         obstacle = ObjectDetection(frame)
         beginning = ObjectDetection(frame)
         end = ObjectDetection(frame)
-        robot = ObjectDetection(frame)
 
         dummy_frame = np.zeros_like(frame)
 
@@ -155,6 +341,19 @@ if __name__ == '__main__':
 
         end.hsv_detection(red_lower, red_upper, 1, 3)
         end.rectangle_drawing(100, 500)
+
+        left_robot = TrajectoryPlanner(frame, beginning, end, obstacle)
+        
+        left_robot.linear_path()
+
+        rot_obs, mid_field, ext_field = left_robot.rotate_obstacles(obstacle.mask)
+
+        obstacle.potential_fields(frame, rot_obs, mid_field, ext_field)
+
+        left_robot.obstacle = obstacle
+        left_robot.draw_trajectory()
+
+        frame = left_robot.frame
         
         try:
             cv2.imshow('Video', frame)
