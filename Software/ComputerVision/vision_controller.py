fdd610d66fac3e1eae0b77deb98b596793033299
pedrodeniz48 <pedrodeniz11@gmail.com>
2023-06-07 13:07:50 -0600

    [0.3.0] Adding multiple obstacles detection

--- Software/ComputerVision/vision_controller.py
+++ Software/ComputerVision/vision_controller.py
@@ -17,7 +17,7 @@ class ObjectDetection:
         self.frame = frame
         self.center = None
 
-    def hsv_detection(self, lower_range, upper_range, erode=None, dilate=None):
+    def hsv_detection(self, lower_range, upper_range, erode=None, dilate=None, multiple=None):
 
         hsv_frame = cv2.cvtColor(self.frame,cv2.COLOR_BGR2HSV)
         
@@ -26,8 +26,38 @@ class ObjectDetection:
         self.mask = cv2.dilate(self.mask, np.ones((dilate,dilate), np.uint8))
 
         self.contours, _ = cv2.findContours(self.mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        if multiple:
+            self.center = []
+
+            num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(self.mask, 4, cv2.CV_32S)
+            
+            self.center = [(int(i[0]), int(i[1])) for i in centroids[1:].tolist()]
+
+            self.mask = []
+            
+            for label in range(0,num_labels-1):
+                fake_mask = np.zeros((labels.shape), dtype=np.uint8)
+
+                fake_mask[labels==label+1] = 255
+                #print(fake_mask.shape)
+                self.mask.append(fake_mask)
+               
+
+    def binary_detection(self, mask, binary_frame, threshold=125, erode=None, dilate=None):
+
+        #blurred = cv2.GaussianBlur(frame, (5, 5), 0)
+        #gray_frame = cv2.cvtColor(blurred,cv2.COLOR_BGR2GRAY)
+
+        _, umbral = cv2.threshold(binary_frame,threshold,255,cv2.THRESH_BINARY)
+
+        #mask = cv2.erode(umbral,np.ones((erode,erode), np.uint8))
+        #mask = cv2.dilate(mask, np.ones((dilate,dilate), np.uint8))
+
+        self.contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         
-    def rectangle_drawing(self, min_area=None, max_area=None, orientation=False):
+    def rectangle_drawing(self, min_area=None, max_area=None, orientation=False, robot=False):
+        self.box = []
         
         for idx, cnt in enumerate(self.contours):
             area = cv2.contourArea(cnt)
@@ -62,44 +92,37 @@ class ObjectDetection:
     
     def potential_fields(self, frame, int_field, mid_field, ext_field, offset=None, offset2=None):
 
-        self.field_mask = np.zeros_like(cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY))
-        
-        if self.box.any():
-            box = self.box
-        else:
-            print("Need to run square_drawing() with orientation first!")
-            return
+        self.mid_field_mask = np.zeros_like(cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY))
+        self.mid_boxes = []
 
-        self.field_mask = np.zeros_like(frame)
+        for obs in range(0,len(self.center)): #obstacle.center
+            self.binary_detection(int_field[obs], 250, 1, 1)
+            self.frame = frame
+            self.rectangle_drawing(1, 1000, True)
 
-        try:
-            #Middle potential field
-            cv2.line(self.frame, (box[0][0]-offset, box[0][1]+offset), (box[1][0]-offset, box[1][1]-offset), (0,255,0), 2)
-            cv2.line(self.frame, (box[1][0]-offset, box[1][1]-offset), (box[2][0]+offset, box[2][1]-offset), (0,255,0), 2)
-            cv2.line(self.frame, (box[2][0]+offset, box[2][1]-offset), (box[3][0]+offset, box[3][1]+offset), (0,255,0), 2)
-            cv2.line(self.frame, (box[3][0]+offset, box[3][1]+offset), (box[0][0]-offset, box[0][1]+offset), (0,255,0), 2)
+            self.binary_detection(mid_field[obs], 250, 1, 1)
+            self.frame = frame
+            self.rectangle_drawing(1, 10000, True)
 
-            #Drawing it in an empty mask
-            cv2.line(self.field_mask, (box[0][0]-offset, box[0][1]+offset), (box[1][0]-offset, box[1][1]-offset), (0,255,0), 2)
-            cv2.line(self.field_mask, (box[1][0]-offset, box[1][1]-offset), (box[2][0]+offset, box[2][1]-offset), (0,255,0), 2)
-            cv2.line(self.field_mask, (box[2][0]+offset, box[2][1]-offset), (box[3][0]+offset, box[3][1]+offset), (0,255,0), 2)
-            cv2.line(self.field_mask, (box[3][0]+offset, box[3][1]+offset), (box[0][0]-offset, box[0][1]+offset), (0,255,0), 2)
+            self.binary_detection(mid_field[obs], 250, 1, 1)
+            self.frame = self.mid_field_mask
+            self.rectangle_drawing(1, 10000, True)
+            self.mid_boxes.append(self.box)
 
-            #External potential field
-            cv2.line(self.frame, (box[0][0]-offset2, box[0][1]+offset2), (box[1][0]-offset2, box[1][1]-offset2), (255, 0,255), 2)
-            cv2.line(self.frame, (box[1][0]-offset2, box[1][1]-offset2), (box[2][0]+offset2, box[2][1]-offset2), (255, 0,255), 2)
-            cv2.line(self.frame, (box[2][0]+offset2, box[2][1]-offset2), (box[3][0]+offset2, box[3][1]+offset2), (255, 0,255), 2)
-            cv2.line(self.frame, (box[3][0]+offset2, box[3][1]+offset2), (box[0][0]-offset2, box[0][1]+offset2), (255, 0,255), 2)
-        except:
-            pass
-
+            self.binary_detection(ext_field[obs], 250, 1, 1)
+            self.frame = frame
+            self.rectangle_drawing(1, 100000, True)
                 
+
 
 
 
 if __name__ == '__main__':
     
     shot = cv2.VideoCapture(2)
+    
+    pink_lower = (165, 125, 125)
+    pink_upper = (175, 255, 255)
 
     green_lower = (50, 200, 0)
     green_upper = (85, 255, 255)
@@ -107,11 +130,25 @@ if __name__ == '__main__':
     red_lower = (0, 150, 150)
     red_upper = (20, 255, 255)
 
-    while True:
+    blue_lower = (120, 140, 75)
+    blue_upper = (140, 255, 175)
 
+    while True:
         ret, frame = shot.read()
+
+        obstacle = ObjectDetection(frame)
         beginning = ObjectDetection(frame)
         end = ObjectDetection(frame)
+        robot = ObjectDetection(frame)
+
+        dummy_frame = np.zeros_like(frame)
+
+        final_frame = frame
+        dummy_frame = np.zeros_like(frame)
+
+        obstacle.hsv_detection(pink_lower, pink_upper, 3, 5, True)
+        obstacle.frame = dummy_frame
+        obstacle.rectangle_drawing(50, 1000, True)
         
         beginning.hsv_detection(green_lower, green_upper, 3, 5)
         beginning.rectangle_drawing(50, 500)
@@ -123,8 +160,6 @@ if __name__ == '__main__':
             cv2.imshow('Video', frame)
         except:
             pass
-
-        #cam_pub.image_pub(topic_name='/Camera', image=video)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
